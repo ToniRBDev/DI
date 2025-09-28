e049d33db68982355b00be62737973ac82931e68
Antonio <OBT59086@educastur.es>
2025-09-28 19:15:06 +0200

    28-9-25

---
+++ 01_JavaScript/js/01-ejemplo.js
@@ -0,0 +1,20 @@
+
+//let y const
+let name = "Antonio";
+console.log(name);
+name = 'Juan';
+console.log(name);
+name=20;
+console.log(name);
+console.log("Hola " + 20);
+const lastName="Alvarez";
+//lastName="Perez"; Es una constante y no se puede modificar su valor.
+let age;
+console.log(age);
+//const year;  --Las constantes siempre tienne que ser inicializadas
+
+//Ejemplos de tipos de datos
+let nombre="Toni";
+let saludo = 'Hola';
+let mensaje = 'Bienvenido ${nombre}';
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# DI
+# DESAROOLLO DE INTERFACES
+
+## Introducción a javaScript para React y Angular
+
+### ¿Cuánto JavaScript debo saber antes de comenzar con Angular o React?
+
+#### Cosas que se deben saber
+
+    - *Let y const*
+    - *Promises*
+    - *Módulos (ESM)*
+    - A
